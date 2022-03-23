ece024bf82769918d59655d5e3d3096757db8522
rodrigonahid <rodrigonahid1@gmail.com>
2022-03-23 08:00:19 -0300

    installing django rest

---
+++ README.md
@@ -0,0 +1,21 @@
+# The Silver Logic Assignment
+
+An API built in Python, using Django Framework.
+Features:
+
+- Registration, login and logout
+- The Wall Dashboard (Authed)
+- The Wall Dashboard (As guest)
+
+## Set Virtual Env
+
+Go to the root folder (same directory as `manage.py`)
+
+If you are on Windows: `venv\Scripts\activate`
+if your are on MacOS or Linux: `source venv/bin/activate`
+
+## Run development server
+
+Run `python manage.py runserver`
+
+The server will be running at `http://127.0.0.1:8000/`
--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,23 @@
+from urllib import request
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
 # Create your views here.
-
 def register(req):
-  return render(req, "authentication/signup.html")
+
+  if req.method == "POST":
+    username = req.POST["username"]
+    email = req.POST["email"]
+    password = req.POST["password"]
+    name = req.POST["name"]
+
+    myuser = User.objects.create_user(username, email, password)
+    myuser.name = name
+    myuser.save()
+
+  return JsonResponse({"req": "req"})
 
 def login(req):
   return render(req, "authentication/login.html")
