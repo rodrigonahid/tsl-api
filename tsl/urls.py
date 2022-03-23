8869402669bbd97d3c363b03935f439103e0d8db
rodrigonahid <rodrigonahid1@gmail.com>
2022-03-23 18:14:06 -0300

    removing app

--- tsl/urls.py
+++ tsl/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("authentication.urls")),
     path('api-auth/', include('rest_framework.urls')),
 ]
