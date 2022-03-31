db549adf7352d3441ae1b528f376ddeb372e1e4e
rodrigonahid <rodrigonahid1@gmail.com>
2022-03-31 19:48:35 -0300

    cleaning project and installing jwt

--- tsl/settings.py
+++ tsl/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'posts',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -131,5 +130,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'users.User'
