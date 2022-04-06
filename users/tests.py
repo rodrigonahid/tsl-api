4fa5ffaeb475bd46705cdc5c1b1006a77a47de76
rodrigonahid <rodrigonahid1@gmail.com>
2022-04-05 21:05:06 -0300

    creating more tests to the user model

--- users/tests.py
+++ users/tests.py
@@ -1,14 +1,28 @@
 from django.test import TestCase
-from django.test import Client
 
 # Create your tests here.
 class UsersTests(TestCase):
-  def test_create_user(self):
-    c = Client()
-    response = c.post("/users/", {
-      "email": "test@test.com",
-      "username": "test",
-      "password": "12345678"
-    })
-    
-    self.assertEqual(response.status_code, 201)
+	def test_create_user(self):
+		response = self.client.post("/users/", {
+			"email": "test@test.com",
+			"username": "test",
+			"password": "12345678"
+		})
+		self.assertEqual(response.status_code, 201)
+
+	def test_create_user_invalid_password(self):
+			response = self.client.post("/users/", {
+				"email": "test@test.com",
+				"username": "test",
+				"password": "123"
+			})
+			self.assertEqual(response.status_code, 400)
+
+	def test_create_user_invalid_username(self):
+			response = self.client.post("/users/", {
+				"email": "test@test.com",
+				"username": "test@* _dsa",
+				"password": "123"
+			})
+			self.assertEqual(response.status_code, 400)		
+			
